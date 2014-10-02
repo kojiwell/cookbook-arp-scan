15c94a8faabec34b8a81470779fdbbf0774cb4e3
Koji Tanaka <kj.tanaka@gmail.com>
2014-10-02 14:40:32 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'arp-scan'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'Apache 2.0'
+description      'Installs/Configures arp-scan'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
